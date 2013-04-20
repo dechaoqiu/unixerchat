696038a24e54adf60a15683a06d44a5194da5a80
crystaldust <juzhenatpku@gmail.com>
2013-04-20 20:54:56 +0800

    Replace the Chinese comments with English.

--- client.c
+++ client.c
@@ -7,50 +7,50 @@
 #include <string.h>
 #define DEST_IP "127.0.0.1"
 #define SERVPORT 6666
-#define MAXDATASIZE 1000 /*每次最大数据传输量 */
+#define MAXDATASIZE 1000 /* The maximum size of the transfered data each time */
 #define BUFFSIZE MAXDATASIZE+1
 int main(int argc, char ** argv){
 	int sockfd,recvbytes;
-	unsigned char buf[BUFFSIZE];//发送缓冲区
+	unsigned char buf[BUFFSIZE];/*The buffer to store the messages to be sent.*/
 	struct sockaddr_in serv_addr;
 	if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1){
-		printf("socket创建出错！\n");
+		printf("Failed to create socket!\n");
 		exit(1);
 	}
-	printf("socket创建成功！\n");
+	printf("Socket created!\n");
 	serv_addr.sin_family=AF_INET;
 	serv_addr.sin_port=htons(SERVPORT);
 	serv_addr.sin_addr.s_addr=inet_addr(DEST_IP);
 	bzero(&(serv_addr.sin_zero),8);
-	printf("准备连接\n");
+	printf("Ready to connect\n");
 	if(connect(sockfd, (struct sockaddr *)&serv_addr,sizeof(struct sockaddr)) == -1) {
-		printf("connect出错！\n");
+		printf("Failed to connect!\n");
 		exit(1);
 	}
-	printf("connect成功！\n");
+	printf("Connected!\n");
 	printf("buf: %d\n",sizeof(buf));
 	memset(buf,0,BUFFSIZE);
 	for(;;){
-		//获取服务器回复
+		/* Receive the reply from the server. */
 		if((recvbytes=recv(sockfd,buf,MAXDATASIZE,0))==-1){
-			printf("recv出错！\n");
+			printf("Failed to receive the reply from server!\n");
 			exit(1);
 		}
 		printf("recvbytes: %d\n",recvbytes);
-		//打印服务器回复
-		printf("服务器说: %s\n",buf);	
+		//Print out the reply
+		printf("The server said: %s\n",buf);	
 		memset(buf,0,BUFFSIZE);
-		//获取用户输入
+		/* Get user input from console. */
 		if(fgets(buf,MAXDATASIZE,stdin)==NULL){
 			exit(1);
 		}
-		//发送数据
+		/* Send data */
 		if(send(sockfd, buf, sizeof(buf)+1, 0) == -1){
-			printf("send出错！\n");
+			printf("Failed to send data!\n");
 		}
-		//printf("send成功！\n");
+
 	}
 	close(sockfd);
-	printf("连接关闭\n");
+	printf("Connection closed!\n");
 	return 0;
 }
--- server.c
+++ server.c
@@ -4,63 +4,63 @@
 #include <sys/types.h>
 #include <sys/unistd.h>
 #include <netinet/in.h>
-#define SERVPORT 6666 /*服务器监听端口号 */
-#define BACKLOG 10 /* 最大同时连接请求数 */
+#define SERVPORT 6666 /*The port that the server listens */
+#define BACKLOG 10 /* The maxium number of connections */
 #define MAXDATASIZE 1000
 #define BUFFSIZE MAXDATASIZE+1
 int main(int argc,char ** argv)
 {
-	int sockfd,client_fd; /*sock_fd：监听socket；client_fd：数据传输socket */
-	struct sockaddr_in my_addr; /* 本机地址信息 */
-	struct sockaddr_in remote_addr; /* 客户端地址信息 */
+	int sockfd,client_fd; /*sock_fd：The socket that listens to, client_fd：Data transfermation socket */
+	struct sockaddr_in my_addr; /* The local address information */
+	struct sockaddr_in remote_addr; /* The client address information */
 	unsigned char buf[MAXDATASIZE];
 	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		printf("socket创建出错！\n");
+		printf("Failed to create socket!\n");
 		exit(1);
 	}
-	printf("socket创建成功！\n");
+	printf("Socket created!\n");
 	my_addr.sin_family=AF_INET;
 	my_addr.sin_port=htons(SERVPORT);
 	my_addr.sin_addr.s_addr=INADDR_ANY;
 	//bzero(&(my_addr.sin_zero),8);
 	if(bind(sockfd,(struct sockaddr *)&my_addr,sizeof(struct sockaddr))==-1){
-		printf("bind出错！\n");
+		printf("Failed to bind!\n");
 		exit(1);
 	}
-	printf("bind成功！\n");
+	printf("Bound successfully!\n");
 	if(listen(sockfd,BACKLOG)==-1){
-		printf("listen出错！\n");
+		printf("Failed to listen!\n");
 		exit(1);
 	}
-	printf("listen成功！\n");
+	printf("Listen to port %n!\n", SERVPORT);
 	while(1){
 		int sin_size = sizeof(struct sockaddr_in);
 		if((client_fd = accept(sockfd,(struct sockaddr *)&remote_addr,&sin_size)) == -1) {
-			printf("accept出错\n");
+			printf("Failed to accept connections!\n");
 			continue;
 		}
 		//printf("received a connection from client %s\n", inet_ntoa(remote_addr.sin_addr));
-		printf("获取到了来自客户端的连接请求!\n");
-	//	printf("xx:%d:\n",strlen("你好,服务器接收到了您的连接请求,连接成功!"));
-		if (!fork()){ /* 子进程代码段 */
-			printf("子进程创建完成,send开始！");
-			if(send(client_fd, "你好,服务器接收到了您的连接请求,连接成功!\n", 61, 0) == -1){
-				printf("send出错！\n");
+		printf("Received the connection from client!\n");
+
+		if (!fork()){ /* Sub thread code block */
+			printf("Child process created! Start sending messages!");
+			if(send(client_fd, "Hi, there, the server received your connection request, you have connected to the server successfully!\n", 61, 0) == -1){
+				printf("Failed to send!\n");
 			}
-			//printf("send成功！\n");
+
 			int recvbytes;
-			//printf("开始获取客户端的输入\n");
+			
 			memset(buf,0,BUFFSIZE);
 			if(recvbytes=recv(client_fd,buf,MAXDATASIZE,0)==-1){
-				printf("recv出错\n");
+				printf("Failed to receive!\n");
 				exit(1);
 			}
-			printf("客户端说:%s size: %d\n",buf,strlen(buf));
+			printf("The client says:%s size: %d\n",buf,strlen(buf));
 			memset(buf,0,BUFFSIZE);
 			close(client_fd);
 			exit(0);
 		}
-		printf("父进程\n");
+		printf("Parent process\n");
 		close(client_fd);
 	}
 	return 0;
